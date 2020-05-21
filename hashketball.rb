1f04ad38c2ed0233cb21566db41316bd037723f9
Julio Chazari <julio.chazari@gmail.com>
2020-05-21 20:46:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,9 @@ def game_hash
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
-      :players => [{}, {}, {}, {}, {}]
+      :players => [
+        {}, {}, {}, {}, {}
+      ]
     }
   }
 
